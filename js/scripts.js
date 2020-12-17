77b5a88eaec518b6dcb1b6616d5b65226f76e8f6
Jordan Hargreaves <jhargreaves231@gmail.com>
2020-12-17 13:18:55 -0800

    Make the webpage work with looping function in JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    
+    const array = ["person1", "person2", "animal", "exclamation", "verb", "noun"]
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    array.forEach(function(element){
+      const input = $("input#" + element).val();
+      $("." + element).text(input);
+    });
 
     $("#story").show();
   });
