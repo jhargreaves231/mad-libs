c792f8a5334ce50afdf67c41b7982a9f0bc97247
Jordan Hargreaves <jhargreaves231@gmail.com>
2020-12-21 17:11:21 -0800

    create an array that counts up to the inputed number.

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Jordan Hargreaves Arrays and Looping_
+
+#### _,Arrays and Looping 12.21.2020_
+
+#### By _**Jordan Hargreaves**_
+
+## Description
+
+_This is a single-input form on a webpage where users can type in a number. This application will return a range of numbers up to the user's input and replace certain digits with text. 
+
+## Setup/Installation Requirements
+
+* _https://jhargreaves231.github.io/Programming-Language-Suggester/_
+* _Double click the link provided above and the form will appear on your web browser. Next, fill out the form input asking for a number and you will be provided a list of values_
+
+Describe: beepBoop()
+Test: "It should return an array with a 0 if the number 0 is inputted"
+Expect(beepBoop(0).toEqual([0]);
+
+
+
+## Support and contact details
+
+_Please contact Jordan Hargreaves through email if there are any issues with the site. Email = jhargreaves231@gmail.com_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_JavaScript_
+_Bootstrap_
+_jQuery_
+
+### License
+
+*This webpage is licensed under the MIT license*
+
+Copyright (c) 2020 **_Jordan Hargreaves_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    
+    const input = $("input#number").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    let numberArray = []
+
+    for (let index = 0; index <= input; index +=1){
+      numberArray.push(index);
+    }
+
+    console.log(numberArray);
 
     $("#story").show();
   });
 });
+
+
+
+
+
+/* $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    
+    const array = ["person1", "person2", "animal", "exclamation", "verb", "noun"]
+
+    array.forEach(function(element){
+      const input = $("input#" + element).val();
+      $("." + element).text(input);
+    });
+
+    $("#story").show();
+  });
+});
+*/
